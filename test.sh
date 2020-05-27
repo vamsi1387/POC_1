a3b87ef7151256d2ea78079dd76085e24fde3538
Vamsi <vamsikrishna_a01@infosys.com>
2020-05-27 04:52:07 +0000

    version 0.1.6

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Variables
+domain="CyberCiti.BIz"
+out=""
+
+##################################################################
+# Purpose: Converts a string to lower case
+# Arguments:
+#   $@ -> String to convert to lower case
+##################################################################
+function to_lower() 
+{
+    local str="$@"
+    local output 
+    echo $str
+    output=$(tr '[A-Z]' '[a-z]'<<<"${str}")
+    echo $output
+}
+
+# invoke the to_lower()
+to_lower "This Is a TEST"
+
+# invoke to_lower() and store its result to $out variable
+out=$(to_lower ${domain})
+
+# Display  back the result from $out
+echo "Domain name : $out"
+
--- versionManagement_method.sh
+++ versionManagement_method.sh
@@ -2,14 +2,13 @@ set -ex
 
 addVersion()
 {
-  i=VERSION
-  val=`cat VERSION`
-  dotCount=`grep '\.' -o $i | wc -l`
+  val=$1
+  dotCount=`echo $1 | tr -cd '.'| wc -c`
   # if there is no version tag yet, let's start at 0.0.0
   if [ -z "$val" ]; then
-     echo "No existing version, starting at 0.0.0"
-     echo "0.0.0" > VERSION
-     exit 1
+     #No existing version, starting at 0.0.0
+     echo "0.0.0"
+     exit    
   fi
   if [ $dotCount -eq 0 ]
   then
@@ -17,7 +16,7 @@ addVersion()
   else
       val=`echo $val | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
   fi
-  echo $val > VERSION
+  echo $val 
 }
 
 containerid=`docker container ls -n 1 -q`
@@ -31,25 +30,27 @@ then
 	git pull
 	# bump version
 	IMAGE=helloworld
-	#docker run -v $(pwd):/home/ec2-user/ vamsi1387/helloworld bash
-	addVersion
-	version=`cat VERSION`
-	echo "version: $version"
+	OLD_VERSION=`docker images | grep $IMAGE | head -1 | awk '{print $2}'`
+	NEW_VERION=$(addVersion $OLD_VERSION)
+	echo "New_Version: $NEW_VERION"
 	# building the image
 	docker build -t $USERNAME/$IMAGE:latest .
 	# tag it
 	git add -A
-	git commit -m "version $version"
-	git tag -a "$version" -m "version $version"
+	git commit -m "version $NEW_VERION"
+	git tag -a "$version" -m "version $NEW_VERION"
 	git push
 	#git push --tags
-	docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+	docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$NEW_VERION
 	# push it
 	docker rmi -f $USERNAME/$IMAGE:latest
-	docker push $USERNAME/$IMAGE:$version
+	docker push $USERNAME/$IMAGE:$NEW_VERION
 
 else
-      addVersion //calling addVersion method for incrementing the image version
-      VERSION=`cat VERSION`
-      docker commit $containerid $USERNAME/$IMAGE:$VERSION
+        #addVersion //calling addVersion method for incrementing the image version
+        #VERSION=`cat VERSION`
+        OLD_VERSION=`docker images | grep $IMAGE | head -1 | awk '{print $2}'`
+        NEW_VERION=`addVersion $OLD_VERSION`
+        echo "New_Version: $NEW_VERION"
+	docker commit $containerid $USERNAME/$IMAGE:$NEW_VERSION
 fi
